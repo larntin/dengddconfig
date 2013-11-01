98035c42a973d625d241b006b5e9f12e24cd71e3
larntin <larntin2002770@gmail.com>
2013-11-01 16:24:38 +0800

    modify vimrc in MacVim

--- vim/full_package-unix-8.05_b2/setup.sh
+++ vim/full_package-unix-8.05_b2/setup.sh
@@ -13,6 +13,10 @@ chmod 777 ~/.vim/toolkit/ctags
 #no!  <M-Right>      <C-Right>
 
 #2.
+#open $VIM/vimrc and add line
+#source ~/.vimrc_ex
+
+#3.
 #open ~/.vimrc_ex and and two lines
 #set gfn=Menlo:h13
 #set fileencodings=ucs-bom,utf-8,cp936,gb18030,big5,euc-jp,euc-kr,latin1
