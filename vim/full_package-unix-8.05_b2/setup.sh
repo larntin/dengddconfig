755d87bc0ccccf0cbba653842e9679e00050d90d
larntin <larntin2002770@gmail.com>
2013-11-01 16:15:40 +0800

    add exVim on MacVim

---
+++ vim/full_package-unix-8.05_b2/setup.sh
@@ -0,0 +1,19 @@
+mkdir ~/.vim
+cp -R ./toolkit ~/.vim
+cp -R ./vimfiles/* ~/.vim/
+cp ./.vimrc_ex ~/
+cp ./my/ctags ~/.vim/toolkit/
+chmod 777 ~/.vim/toolkit/ctags
+
+#1.
+#open $VIM/gvimrc and comment out
+#no   <M-Left>       <C-Left>
+#no!  <M-Left>       <C-Left>
+#no   <M-Right>      <C-Right>
+#no!  <M-Right>      <C-Right>
+
+#2.
+#open ~/.vimrc_ex and and two lines
+#set gfn=Menlo:h13
+#set fileencodings=ucs-bom,utf-8,cp936,gb18030,big5,euc-jp,euc-kr,latin1
+
